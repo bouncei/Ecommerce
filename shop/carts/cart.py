ef164e60f84053ccadba1d441a7479717208a727
Joshua inyang <joshuainyang255@gmail.com>
2021-11-09 10:49:04 +0100

    add to cart

--- shop/carts/cart.py
+++ shop/carts/cart.py
@@ -1,15 +1,16 @@
+import re
 from flask import request, render_template, redirect, session, current_app, flash
 from shop import db, app
 from shop.products.models import Addproduct
 
 
-# def MagerDict(dict1, dict2):
-#     if isinstance(dict, list) and isinstance(dict2, list):
-#         return dict1 + dict2
+def MagerDict(dict1, dict2):
+    if isinstance(dict1, list) and isinstance(dict2, list):
+        return dict1 + dict2
     
-#     elif isinstance(dict1, dict) and isinstance(dict2, dict):
-#         return dict(list(dict.items() + list(dict2.items())))
-#     return False
+    elif isinstance(dict1, dict) and isinstance(dict2, dict):
+        return dict(list(dict1.items() + list(dict2.items())))
+    return False
 
 @app.route('/addcart', methods=["POST"])
 def add_cart():
@@ -21,24 +22,50 @@ def add_cart():
         print(quantity,colors)
 
         product = Addproduct.query.filter_by(id=product_id).first()
-        # if product_id and quantity and colors and request.method == "POST":
-        #     dicItems = {product_id: {'name': product.name, 'price': product.price,
-        #     'discount': product.discount, 'colors': colors, 'quantity': quantity, 'image': product.image_1}}
+        string_product_price = str(product.price)
+        
+        if request.method == "POST":
+            dicItems = {product_id: {'name': product.name, 'price': string_product_price,
+            'discount': product.discount, 'colors': colors, 'quantity': quantity, 'image': product.image_1}}
 
-        #     if 'Shoppingcart' in session:
-        #         print(session['Shoppingcart'])
+            print(dicItems)
 
-                # if product_id in session['Shoppingcart']:
-                #     print('This product is already in cart')
-                #     flash('This product is aldredy in cart', 'error')
-                # else:
-                #     session['Shoppingcart'] = MagerDict(session['Shoppingcart'], dicItems)
-                #     flash(f"{product.name} has succesfully been added to cart", 'success')
-                #     return redirect(request.referrer)
+            if 'Shoppingcart' in session:
+                print(session['Shoppingcart'])
+
+                if product_id in session['Shoppingcart']:
+                    print('This product is already in cart')
+                    flash('This product is aldredy in cart', 'error')
+                else:
+                    session['Shoppingcart'] = MagerDict(session['Shoppingcart'], dicItems)
+                    flash(f"{product.name} has succesfully been added to cart", 'success')
+                    return redirect(request.referrer)
 
             # else:
+                print("nothing Here Boss")
+                session['Shoppingcart'] = dicItems
+                return redirect(request.referrer)
+
+
+
+            # if 'Shoppingcaart' not in session:
+                
             #     session['Shoppingcart'] = dicItems
             #     return redirect(request.referrer)
+
+
+            # else:
+            #     print(session['Shoppingcart'])
+
+            #     if product_id in session['Shoppingcart']:
+            #         print("This product is already in cart")
+            #         # flash("", "success")
+
+            #     else:
+            #         session['Shoppingcart'] = MagerDict(session['Shoppingcart'], dicItems)
+            #         flash(f"{product.name} has succesfully been added to cart", 'success')
+            #         return redirect(request.referrer)
+
 
     except Exception as e:
         print(e)
